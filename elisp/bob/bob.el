a5906acd800b47bc19dc24aa90873417e33312e2
panarinm <x.xneorlinx.x@gmail.com>
2019-08-08 19:21:35 +0300

    [ELISP] 2nd iteration of Bob.el

--- elisp/bob/bob.el
+++ elisp/bob/bob.el
@@ -4,30 +4,35 @@
 
 ;;; Code:
 
-(defun yelled? (input)
+(defun yelled-p (input)
+  "Whether string is considered yelled."
   (if
       (not (string= (upcase input) (downcase input)))
       (string= (upcase input) input)
     nil))
 
-(defun yelled-question? (input)
+(defun yelled-question-p (input)
+  "Whether string is a question and yelled."
   (and
-   (yelled? input)
+   (yelled-p input)
    (string-suffix-p "?" input)))
 
-(defun simple-question? (input)
+(defun simple-question-p (input)
+  "Whether string is a simple question."
   (string-suffix-p "?" input))
 
-(defun silence? (input)
+(defun silence-p (input)
+  "Whether string is empty."
   (eq (string-trim input) ""))
 
 (defun response-for (string)
+  "Generate a Bob response to the string."
   (let ((input (string-trim string)))
     (cond
-     ((yelled-question? input) "Calm down, I know what I'm doing!")
-     ((yelled? input) "Whoa, chill out!")
-     ((simple-question? input) "Sure.")
-     ((silence? input) "Fine. Be that way!")
+     ((yelled-question-p input) "Calm down, I know what I'm doing!")
+     ((yelled-p input) "Whoa, chill out!")
+     ((simple-question-p input) "Sure.")
+     ((silence-p input) "Fine. Be that way!")
      (t "Whatever."))))
 
 (provide 'bob)
