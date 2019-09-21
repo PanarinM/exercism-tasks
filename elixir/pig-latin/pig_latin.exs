fd4885c38c0600cf5daa8a279b25f022921cfb82
panarinm <x.xneorlinx.x@gmail.com>
2019-09-21 14:20:10 +0300

    [ELIXIR] some update to pig-latin

--- elixir/pig-latin/pig_latin.exs
+++ elixir/pig-latin/pig_latin.exs
@@ -13,7 +13,36 @@ defmodule PigLatin do
 
   Some groups are treated like vowels, including "yt" and "xr".
   """
+  @vowels ["a", "e", "i", "o", "u", "xr", "yt"]
+  @consonants ["qu", "ch", "squ", "th", "sch", "b", "c", "d", "f", "g", "h",
+  "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z", ]
+
   @spec translate(phrase :: String.t()) :: String.t()
   def translate(phrase) do
+    cond do
+      start_wovel?(phrase) -> phrase <> "ay"
+      cons = start_consonant?(phrase) -> move_consonant(phrase, cons)
+    end
+  end
+
+  defp start_wovel?(phrase) do
+    Enum.any?(@vowels, fn vowel ->
+      String.starts_with?(phrase, vowel)
+    end)
+  end
+
+  defp start_consonant?(phrase) do
+    Enum.reduce_while(@consonants, List.first(@consonants), fn x, acc ->
+      case String.starts_with?(phrase, x) do
+        true -> {:halt, x}
+        _ -> {:cont, false}
+      end
+    end)
+  end
+
+  defp move_consonant(phrase, cons) do
+    String.slice(phrase, String.length(cons)..-1)
+    |> Kernel.<>(cons)
+    |> Kernel.<>("ay")
   end
 end
