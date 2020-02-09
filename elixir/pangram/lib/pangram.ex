e2967b06dfc9561164d9d455139e9c3773b4aebd
panarinm <x.xneorlinx.x@gmail.com>
2020-02-09 20:55:57 +0200

    [ELIXIR] 2nd pangram iteration

--- elixir/pangram/lib/pangram.ex
+++ elixir/pangram/lib/pangram.ex
@@ -1,10 +1,6 @@
 defmodule Pangram do
-  @alphabet [
-    "a", "b", "c", "d", "e", "f", "g",
-    "h", "i", "j", "k", "l", "m", "n",
-    "o", "p", "q", "r", "s", "t", "u",
-    "v", "w", "x", "y", "z",
-  ]
+  @alphabet ?a..?z
+
   @doc """
   Determines if a word or sentence is a pangram.
   A pangram is a sentence using every letter of the alphabet at least once.
@@ -20,14 +16,8 @@ defmodule Pangram do
 
   @spec pangram?(String.t()) :: boolean
   def pangram?(sentence) do
-    sentence = String.downcase(sentence) |> String.graphemes
+    sentence = String.downcase(sentence) |> String.to_charlist
 
-    Enum.reduce_while(@alphabet, true, fn letter, acc ->
-      if letter not in sentence do
-        {:halt, false}
-      else
-        {:cont, acc}
-      end
-    end)
+    Enum.all?(@alphabet, fn x -> x in sentence end)
   end
 end
