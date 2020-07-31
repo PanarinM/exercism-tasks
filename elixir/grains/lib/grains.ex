33dac1858cb97ff1ee2ea1bce6e0aedc9b9f299a
panarinm <x.xneorlinx.x@gmail.com>
2020-07-31 14:23:06 +0300

    [ELIXIR] grains in progress

---
+++ elixir/grains/lib/grains.ex
@@ -0,0 +1,22 @@
+defmodule Grains do
+  @doc """
+  Calculate two to the power of the input minus one.
+  """
+  @spec square(pos_integer) :: {:ok, pos_integer} | {:error, String.t}
+  def square(number) when number >= 1 and number <= 64 do
+    {:ok, :math.pow(2, number - 1) |> trunc}
+  end
+  def square(_), do: {:error, "The requested square must be between 1 and 64 (inclusive)"}
+
+  @doc """
+  Adds square of each number from 1 to 64.
+  """
+  @spec total :: pos_integer
+  def total do
+    {:ok, Enum.map(1..64, fn x ->
+        {:ok, val} = square(x)
+        val
+      end)
+      |> Enum.sum}
+  end
+end
