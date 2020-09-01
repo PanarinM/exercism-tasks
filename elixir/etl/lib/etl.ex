e5e0de6c39220c5ca908ddfbbc621e4fe9d4fc5b
panarinm <x.xneorlinx.x@gmail.com>
2020-09-01 21:12:01 +0300

    [ELIXIR] ETL done

---
+++ elixir/etl/lib/etl.ex
@@ -0,0 +1,18 @@
+defmodule ETL do
+  @doc """
+  Transform an index into an inverted index.
+
+  ## Examples
+
+  iex> ETL.transform(%{"a" => ["ABILITY", "AARDVARK"], "b" => ["BALLAST", "BEAUTY"]})
+  %{"ability" => "a", "aardvark" => "a", "ballast" => "b", "beauty" =>"b"}
+  """
+  @spec transform(map) :: map
+  def transform(input) do
+    Enum.reduce(input, %{}, fn {key, val}, acc ->
+      Enum.reduce(val, acc, fn elem, acc ->
+        Map.put(acc, String.downcase(elem), key)
+      end)
+    end)
+  end
+end
